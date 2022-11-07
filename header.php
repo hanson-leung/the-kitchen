d6f1ed64bef2ed5dfefc2dcc1889825a921d901c
Hanson Leung <53881772+hanson-leung@users.noreply.github.com>
2022-11-06 17:50:09 -0800

    add functioning search and results, css styles too

---
+++ header.php
@@ -0,0 +1,17 @@
+<div class="header">
+    <div>
+        <p>Menu</p>
+    </div>
+    <div  class="grid-center">
+        <a href="/index.php"><img class="logo" src="/uploads/branding/logo.png" alt="logo"/></a>
+    </div>
+    <div class="grid-columns grid-end grid-gap-2rem">
+        <div id="login" >
+            <a href="#" id="loginbutton">Login</a>
+        </div>
+        <div id="signup" >
+            <button id="signup">Sign Up</button>
+        </div>
+    </div>
+</div>
+<!--close header-->
