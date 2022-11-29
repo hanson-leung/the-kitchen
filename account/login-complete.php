e8b2bac36a2f619074512f1f9b11c43f082b7075
Hanson Leung <53881772+hanson-leung@users.noreply.github.com>
2022-11-28 20:24:54 -0800

    fix links

--- account/login-complete.php
+++ account/login-complete.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
-include $_SERVER['DOCUMENT_ROOT'] . '/logic/link_logic.php';
+// check if on localhost or on server
+if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'){
+    $link = "";
+} else {
+    include $_SERVER['DOCUMENT_ROOT'] . '/logic/link_logic.php';
+    $link = "<?php echo $link ?>";
+}
 
 include $_SERVER['DOCUMENT_ROOT'] . '/logic/db-connect.php';
 
--- account/signup-complete.php
+++ account/signup-complete.php
@@ -1,5 +1,11 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/logic/link_logic.php';
+// check if on localhost or on server
+if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'){
+    $link = "";
+} else {
+    include $_SERVER['DOCUMENT_ROOT'] . '/logic/link_logic.php';
+    $link = "<?php echo $link ?>";
+}
 include $_SERVER['DOCUMENT_ROOT'] . '/logic/db-connect.php';
 
 // check if same email exists
