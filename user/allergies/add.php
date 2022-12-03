eeaee046bea9073b36c2241f417ac76c4aa4061e
alexiskim0 <95878478+alexiskim0@users.noreply.github.com>
2022-12-02 17:45:39 -0800

    Update add.php

--- user/allergies/add.php
+++ user/allergies/add.php
@@ -11,11 +11,15 @@ if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'){
 
 <html>
 <!-- begin header -->
+
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/logic/db-connect.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/logic/login_check.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 ?>
+
+<!-- additional stylesheets -->
+<link rel="stylesheet" href="<?php echo $link ?>stylesheets/allergies.css"/>
 
 <!-- begin body -->
 <body>
@@ -35,8 +39,11 @@ if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'){
                 }
                 ?>
             </select>
+            <br>
+            <br>
             <input type="hidden" name="user_id" value="<?php echo $_SESSION["user_id"] ?>">
-            <input type="submit" value="Add Allergy">
+            <br>
+            <input type="submit" value="Add" style = "width:100px;">
         </form>
     </div>
 </body>
